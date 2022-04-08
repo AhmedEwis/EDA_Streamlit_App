713c4424cc534edcd9353c40153a12b108ba493d
Ahmed Ewis <65680238+AhmedEwis@users.noreply.github.com>
2022-04-08 20:41:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ dataset = st.file_uploader(label = '')
 
 use_defo = st.checkbox('Use example Dataset')
 if use_defo:
-    dataset = 'Interpolation_Average_2013_2015.xlsx'
+    dataset = 'Interpolation_Average_2013_2015 (1).csv'
 
 st.sidebar.header('Import Dataset to Use Available Features: 👉')
 
